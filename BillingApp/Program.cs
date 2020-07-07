249ae5cdc98245deb90c5eee50f2ed7ecb674cff
Santosh Mohapatra <Santosh.Mohapatra2@Honeywell.com>
2020-07-07 20:14:29 +0530

    first commit

---
+++ BillingApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BillingApp
+{
+    public class PromoEngine
+    {
+
+        public void Display()
+        {
+            Console.Write("Hallo World! ");
+        }       
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 1 for new order or 0 for Exit: ");
+        }
+    }
+}
